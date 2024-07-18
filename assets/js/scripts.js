f74d700156129c3266c98810acffa7d5be5a4795
MARIA CRISTINA PEREZ DE ARCE <cristinacharme18@gmail.com>
2024-07-18 19:50:52 -0400

    Se realiza carrusel de imágenes.

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+$(function () {
+    var win = $(this)
+    var tooltipTriggerList = [].slice.call(
+       document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    )
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+       return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+ 
+    /*----------  Navbar ----------*/
+ 
+    if (win.width() > 576) {
+       $(".navbar").addClass("fixed-top")
+    }
+ 
+    $(window).on("resize", function () {
+       if (win.width() > 576) {    
+          $(".navbar").addClass("fixed-top")
+       } else {
+          $(".navbar").removeClass("fixed-top")
+       }
+    })
+ })
+ 
