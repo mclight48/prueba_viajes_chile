50b425233b8aa7c081eebaadb80245f3d7078c0f
MARIA CRISTINA PEREZ DE ARCE <cristinacharme18@gmail.com>
2024-07-18 21:37:33 -0400

    Se trabaja en sección Contacto diseñando con Bootstrap un formulario de contacto, el cual se usa un mensaje de alerta al oprimir el botón Enviar.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,33 @@
 $(function () {
-    var win = $(this)
-    var tooltipTriggerList = [].slice.call(
-       document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    )
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-       return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
- 
-    /*----------  Navbar ----------*/
- 
-    if (win.width() > 576) {
-       $(".navbar").addClass("fixed-top")
-    }
- 
-    $(window).on("resize", function () {
-       if (win.width() > 576) {    
-          $(".navbar").addClass("fixed-top")
-       } else {
-          $(".navbar").removeClass("fixed-top")
-       }
-    })
- })
- 
+   var win = $(this)
+   var tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+   )
+   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+   })
+
+   /*----------  Navbar ----------*/
+
+   if (win.width() > 576) {
+      $(".navbar").addClass("fixed-top")
+   }
+
+   $(window).on("resize", function () {
+      if (win.width() > 576) {
+         $(".navbar").addClass("fixed-top")
+      } else {
+         $(".navbar").removeClass("fixed-top")
+      }
+   })
+
+
+   /*---------- Aviso Alerta  ----------*/
+
+   $(".btn").click(function (event) {
+      event.preventDefault()
+      $(".alert").show("")
+   })
+})
+
+
